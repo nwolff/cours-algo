1fbae4a4c720496459b4bacadcbe8c7cdef805bf
WOLFF  Nicholas <wolff_n@b102-im0b.pedago.gybe>
2022-11-09 15:42:01 +0100

    simpler

--- galets/galets.py
+++ galets/galets.py
@@ -1,31 +1,25 @@
 from collections import defaultdict
 from itertools import cycle
 
-connectivity = [
-    (0, 1),
-    (0, 2),
-    (0, 4),
-    (0, 9),
-    (1, 3),
-    (1, 5),
-    (2, 3),
-    (3, 6),
-    (4, 5),
-    (4, 8),
-    (4, 10),
-    (5, 6),
-    (6, 7),
-    (6, 8),
-    (7, 9),
-    (8, 9)
-]
+connectivity = {
+    0: [1, 2, 4, 9],
+    1: [3, 5, 9],
+    2: [3],
+    3: [6],
+    4: [5, 8, 10],
+    5: [6],
+    6: [7, 8],
+    7: [9],
+    8: [9]
+}
 
 
 def build_connectivity_map():
     m = defaultdict(set)
-    for a, b in connectivity:
-        m[a].add(b)
-        m[b].add(a)
+    for a, elems in connectivity.items():
+        for b in elems:
+            m[a].add(b)
+            m[b].add(a)
     return m
 
 
